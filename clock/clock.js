7df7e38dc4535edf76c6a71bc0e86109b693762d
Mia <42193533+Miaamen@users.noreply.github.com>
2020-02-24 23:07:14 +0800

    Create clock.js

---
+++ clock/clock.js
@@ -0,0 +1,28 @@
+var vm = new Vue({
+	el : '#clock',
+	data : {
+		date : '',
+		time : '',
+		idol : 'Good good study , day day up',
+		colorNum : 'color-1'
+	}
+});
+
+var week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+var timerID = setInterval(updateTime, 1000);
+
+updateTime();
+
+function updateTime(){
+	var currDate = new Date();
+	vm.date = addZero(currDate.getFullYear(),4) + '-' + addZero(currDate.getMonth() + 1,2) + '-' + addZero(currDate.getDate(),2) + ' ' + week[currDate.getDay()];
+	vm.time = addZero(currDate.getHours(),2) + ':' + addZero(currDate.getMinutes(),2) + ':' + addZero(currDate.getSeconds(),2);
+} 
+
+function addZero(num,digit){
+	var zero = '';
+	for(let i = 0; i < digit; i ++){
+		zero += '0';
+	}
+	return (zero + num).slice(-digit);
+}
